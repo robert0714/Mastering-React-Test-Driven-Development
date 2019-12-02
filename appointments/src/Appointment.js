48685575346e7b7231c2ee510381136ac51a7cbc
robert.lee <robert0714@gmail.com>
2019-12-02 16:12:09 +0800

    refactoring clean test

---
+++ appointments/src/Appointment.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+export const Appointment = ({customer}) => 
+  <div>{customer.firstName}</div>;
--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,7 +1,31 @@
-import { isTSAnyKeyword } from "@babel/types";
+import { isTSAnyKeyword, exportAllDeclaration } from "@babel/types";
+import ReactDOM from 'react-dom';
+import React from 'react';
+import { Appointment } from "../src/Appointment";
 
-describe('Appointments' , () =>{
-   it('renders the customer first name', () =>{
 
+
+describe('Appointment' , () =>{
+   let container;
+   let customer;
+   let render ;
+ 
+   beforeEach(() => {
+     container = document.createElement('div');
+     render = (component) => ReactDOM.render(component , container );
    });
+    
+
+   it('renders the customer first name', () =>{
+      customer = { firstName: 'Ashley' } ;
+      render(<Appointment customer={customer} />) ;
+      expect(container.textContent).toMatch('Ashley');
+   });
+   
+   it ('renders another customer first name',() =>{
+	customer = { firstName: 'Jordan' } ;
+      render(<Appointment customer={customer} />) ;
+      expect(container.textContent).toMatch('Jordan');
+
+   })
 });
