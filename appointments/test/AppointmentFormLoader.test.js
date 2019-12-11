4ea048db6d20075f800d64ee28953f7add21ff68
robert.lee <robert0714@gmail.com>
2019-12-11 14:38:06 +0800

    fixbug about wrong word availableTimeSlots not avalaibleTimeSlot

--- appointments/test/AppointmentFormLoader.test.js
+++ appointments/test/AppointmentFormLoader.test.js
@@ -24,10 +24,11 @@ describe('AppointmentFormLoader', () => {
     window.fetch.mockRestore();
     AppointmentFormExports.AppointmentForm.mockRestore();
   });
-  it('fetch data when component is mounted', () => {
-    render(<AppointmentFormLoader />);
-    expect(window.fetch).toHaveBeenCalledWidth(
-      '/avalaibleTimeSlots',
+  it('fetches data when component is mounted', async () => {
+    await renderAndWait(<AppointmentFormLoader />);
+
+    expect(window.fetch).toHaveBeenCalledWith(
+      '/availableTimeSlots',
       expect.objectContaining({
         method: 'GET',
         credentials: 'same-origin',
@@ -35,22 +36,25 @@ describe('AppointmentFormLoader', () => {
       })
     );
   });
-  it('initially passes no data to AppointmentForm', () => {
-    render(<AppointmentFormLoader />);
+
+  it('initially passes no data to AppointmentForm', async() => {
+    await renderAndWait(<AppointmentFormLoader />);
     expect(
       AppointmentFormExports.AppointmentForm
-    ).toHaveBeenCalledWidth(
-      { avalaibleTimeSlots: [] },
+    ).toHaveBeenCalledWith(
+      { availableTimeSlots: [] },
       expect.anything()
     );
+     
   });
+   
 
   it('displays time slots that are fetched on mount', async () => {
     await renderAndWait(<AppointmentFormLoader />);
     expect(
       AppointmentFormExports.AppointmentForm
-    ).toHaveBeenCalledWidth(
-      { avalaibleTimeSlots },
+    ).toHaveBeenCalledWith(
+      { availableTimeSlots },
       expect.anything()
     );
   });
