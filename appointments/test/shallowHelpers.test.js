fd348e4f80149a5928d9e571f6fd96fe364794b0
robert.lee <robert0714@gmaill.com>
2019-12-15 14:49:43 +0800

    p167 Listing element children

---
+++ appointments/test/shallowHelpers.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { childrenOf } from "./shallowHelpers";
+describe('childrenOf' ,()=>{
+    it('it returns no children' ,()=> {
+        expect(childrenOf(<div />)).toEqual([]);
+    });
+    it('returns direct children', ()=>{
+        expect(childrenOf(
+            <div>
+                <p>A</p>
+                <p>B</p>
+            </div>
+        )).toEqual([<p>A</p>, <p>B</p> ]);
+    });
+    it('returns a text as an array of one item', ()=>{
+        expect(
+            childrenOf( <div>text</div>)
+        ).toEqual(['text']);
+    });
+    it('returns no children for text' , ()=>{
+        expect(childrenOf('text')).toEqual([]);
+    });
+    it('returns array of children for elements with one child',()=>{
+        expect(childrenOf(
+            <div>
+                <p>A</p>
+            </div>
+        )).toEqual([<p>A</p> ]);
+    });
+});
