6432f2958dd04340cbdbc2a788d061b6c7bca429
robert.lee <robert0714@gmail.com>
2019-12-16 14:29:59 +0800

    building a new root component - part05

--- appointments/src/App.js
+++ appointments/src/App.js
@@ -1,13 +1,26 @@
 import React, { useState, useCallback } from 'react';
 import ReactDOM from 'react-dom';
 import { AppointmentsDayViewLoader } from './AppointmentsDayViewLoader';
-export const App = () => (
-  <React.Fragment>
-    <div className ="button-bar">
-        <button type ="button"  id ="addCustomer" >
-            Add customer and appointment
+import CustomerForm from './CustomerForm';
+
+export const App = () => {
+  const [view, setView] = useState('dayView');
+  const transitionToAddCustomer = useCallback(
+    () => setView('addCustomer'),
+    []
+  );
+  return (
+    <React.Fragment>
+      <div className="button-bar">
+        <button
+          type="button"
+          id="addCustomer"
+          onClick={transitionToAddCustomer}>
+          Add customer and appointment
         </button>
-    </div>
-    <AppointmentsDayViewLoader></AppointmentsDayViewLoader>
-  </React.Fragment>
-);
+      </div>
+      <AppointmentsDayViewLoader></AppointmentsDayViewLoader>
+      {/* {view === 'addCustomer' ? <CustomerForm /> : null} */}
+    </React.Fragment>
+  );
+};
